19be36a3bd492b1421c4d31fde6d6b55bd766180
timoteewo404 <byansianthony@gmail.com>
2025-11-10 17:31:22 +0300

    Switch to PostgreSQL: update schema provider, .env, and remove old migrations

--- prisma/migrations/20251023144515_add_podcast_report_fields/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "news_events" ADD COLUMN "audioUrl" TEXT;
-ALTER TABLE "news_events" ADD COLUMN "author" TEXT;
-ALTER TABLE "news_events" ADD COLUMN "podcastDuration" TEXT;
-ALTER TABLE "news_events" ADD COLUMN "podcastPlatforms" TEXT;
-ALTER TABLE "news_events" ADD COLUMN "reportUrl" TEXT;
-ALTER TABLE "news_events" ADD COLUMN "role" TEXT;
